f04e63e227b226ccb3fb425e81dd5d8b3d4e6320
furkan <furkanm14@gmail.com>
2026-04-07 15:33:52 +0530

    changed api url

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api';
+// const API_BASE_URL = '/api';
+const API_BASE_URL = 'https://my-json-server.typicode.com/furkanmemon/react-job-portal';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
